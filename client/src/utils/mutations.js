2afa2f180335f856f8ca80d83399c75b9f598f39
jermz678 <jermz678@live.com>
2021-07-24 19:24:04 -0500

    wrote mutations file and made adjustments to the resolvers

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookId: ID!){
+        saveBook(bookId: $bookId){
+            _id
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+    `;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($bookId: ID!){
+        deleteBook(bookid: $bookId){
+            _id
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+`;
