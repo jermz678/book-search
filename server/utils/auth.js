c5ed6146198dfb23678d165a16b32ad9302f0665
jermz678 <jermz678@live.com>
2021-07-26 20:43:14 -0500

    trying to fix heroku from not working with server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,14 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+
+  signToken: function ({ username, email, _id }) {
+    console.log(username, email, _id)
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   // function for our authenticated routes
   authMiddleware: function ({req, res, next}) {
     // allows token to be sent via  req.query or headers
@@ -24,16 +32,8 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    console.log(username, email, _id)
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+    return req;
+  }
 };
